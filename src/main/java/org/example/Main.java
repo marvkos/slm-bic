d9d2165de22bfafb6c7fa50c706eaab22992681a
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2023-10-04 18:59:10 +0200

    add calculator class and add function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(2, 6);
+
+        System.out.println(result);
     }
 }
